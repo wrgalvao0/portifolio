cd5bb45b5cb1d1c2952caf8455c5ca690cd1dc2c
wrgalvao0 <warleygalvao61@gmail.com>
2025-03-27 10:10:58 -0300

    Atualização do codigo emailjs e correção de bugs

--- js/email/email.js
+++ js/email/email.js
@@ -1,16 +1,15 @@
-// Inicializa o EmailJS com a sua chave de API
-emailjs.init("809i1U5skqwGS0ddoT1ux"); // Substitua pela chave gerada no painel do EmailJS
+emailjs.init("8OqhN0y-l5lqa5FlN"); // Substitua pela chave gerada no painel do EmailJS
 
 // Função para enviar o e-mail
 document.getElementById("form-contato").addEventListener("submit", function (e) {
   e.preventDefault();
 
   const formData = {
-    from_name: document.getElementById("email").value,
-    from_email: document.getElementById("whatsapp").value,
-    message: document.getElementById("mensagem").value,
+    nome: document.getElementById("nome").value,
+    email: document.getElementById("email").value,
+    mensagem: document.getElementById("mensagem").value,
   };
-
+  
   emailjs.send("service_a6z9his", "template_q2vnpp4", formData)
     .then(function(response) {
       console.log('Mensagem enviada com sucesso!', response);
